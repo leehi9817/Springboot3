305ebaade1a00926972d3303e4c5f4426a198eca
이혜인 <leehi9817@gmail.com>
2024-08-29 21:11:33 +0900

    2.2.3 모델 추가하기

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -4,12 +4,16 @@ package com.example.firstproject.controller;
 import org.springframework.stereotype.Controller;
 // URL 연결 요청(@GetMapping())과 동시에 자동으로 임포트
 import org.springframework.web.bind.annotation.GetMapping;
+// Model 클래스 패키지 자동 임포트
+import org.springframework.ui.Model;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) { // Model 객체 받아오기
+        // model 객체가 "혜인" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "hyein"); // "혜인" → "hyein" 수정
         return "greetings"; // greetings.mustache 파일 반환
     }
 }
